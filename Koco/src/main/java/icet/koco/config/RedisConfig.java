66acc51501341e812ecc3c03e13292b6f09c848d
ywoo121 <71367047+ywoo121@users.noreply.github.com>
2025-07-09 01:32:38 +0900

    Feat: RedisConfig 직렬화 가능하도록

--- Koco/src/main/java/icet/koco/config/RedisConfig.java
+++ Koco/src/main/java/icet/koco/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package icet.koco.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
