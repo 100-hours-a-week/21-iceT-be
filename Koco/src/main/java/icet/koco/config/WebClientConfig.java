fcdd9d6bfbc54d1566e247673817e7d8896b4e4f
ywoo121 <ywoo121@naver.com>
2025-07-11 14:05:14 +0900

    Chore: TEST용 WebClientConfig 생성

--- Koco/src/main/java/icet/koco/config/WebClientConfig.java
+++ Koco/src/main/java/icet/koco/config/WebClientConfig.java
@@ -3,10 +3,12 @@ package icet.koco.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@Profile("!test")
 @Configuration
 public class WebClientConfig {
 
--- Koco/src/test/java/icet/koco/KocoApplicationTests.java
+++ Koco/src/test/java/icet/koco/KocoApplicationTests.java
@@ -1,13 +1,20 @@
 package icet.koco;
 
+import icet.koco.auth.TestWebClientConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
+import java.util.TimeZone;
+
+@ActiveProfiles("test")
 @SpringBootTest
+@Import(TestWebClientConfig.class)
 class KocoApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
